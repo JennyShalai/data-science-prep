7f8bde7abf1632c51c79ae53785c4b2e735a47c6
Jenny Shalai <jenny.shalai@gmail.com>
2019-06-22 08:51:55 -0400

    add solution for challenge 5

--- functions.py
+++ functions.py
@@ -99,3 +99,25 @@ def next_perfect_square(number):
         return (n+1)*(n+1)
     else:
         return -1
+
+
+# Challenge 5:
+import random
+def flip_coin(p=0.5):
+    '''
+        Using random.random() simulate flipping a coin where the probability of
+        flipping a head is p.
+        
+        Parameters
+        ----------
+        p: {float} probability of flipping a head (must be between 0 and 1)
+        
+        Returns
+        -------
+        flip: {str} "H" if coin is heads, otherwise "T"
+        '''
+    n = random.random()
+    if n < p:
+        return 'H'
+    else:
+        return 'T'
