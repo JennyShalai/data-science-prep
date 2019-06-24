2efa47f7630c61f3c9328db3a3454df6585fa41e
Jenny Shalai <jenny.shalai@gmail.com>
2019-06-24 17:46:00 -0400

    add solution challenge 2

--- python-numpy.py
+++ python-numpy.py
@@ -23,4 +23,27 @@ def elements_twice_min(arr):
 
 
 # Challenge 2:
+# Add two NumPy vectors or matrices together, if possible.
+# If it is not possible to add the two vectors/matrices together
+# (because their sizes differ), return False.
 
+import numpy as np
+
+def mat_addition(A, B):
+    """
+        Add vector/matrix arrays A and B together. If they cannot be added
+        return False.
+        
+        Parameters
+        ----------
+        A: NumPy array size of (n,) or (n, m)
+        B: NumPy array size of (p,) or (p, q)
+        
+        Returns
+        -------
+        NumPy Array of same size as A and B, or False if their sizes differ.
+        """
+    if A.shape == B. shape:
+        return A + B
+    else:
+        return False
