e86c1ebb9a8727cea1e62dcf4bf6c992a97d5975
Jenny <jshalai@intrepid.io>
2019-07-10 20:19:31 -0400

    add query solution for task 2

--- queries.sql
+++ queries.sql
@@ -4,3 +4,10 @@
 SELECT appearance
 FROM animals
 WHERE species = "rat";
+
+/* Use IN to show the species for animals with vertebrate_class 'mammal' and 'amphibian' */
+SELECT species
+FROM animals
+WHERE vertebrate_class IN ('mammal', 'amphibian');
+
+
